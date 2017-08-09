e53ba8fe13ff84969f9090cbb09f8b9f23184430
Carter De Angelis <carterdea@gmail.com>
2017-08-09 04:36:25 -0700

    Some tablet + desktop styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,18 @@ $(document).ready(function(){
   $(".menu-link").click(function(elm) {
     elm.preventDefault();
     $("header nav").toggle();
+
+    var icon = $(".menu-link img");
+    var iconSrc = icon.attr("src");
+    var menu = "assets/img/icon-menu.svg";
+    var close = "assets/img/icon-close.svg";
+
+    if (iconSrc === menu) {
+      icon.attr("src", close);
+    } else {
+      icon.attr("src", menu);
+    }
+
   });
 
 });
